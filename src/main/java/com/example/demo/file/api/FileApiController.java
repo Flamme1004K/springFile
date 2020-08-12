8571436ed3667b2abcf2f339780c447dad4bd768
flamme <flamme1004@gmail.com>
2020-08-13 00:16:07 +0900

    binaryFileInput

--- src/main/java/com/example/demo/file/api/FileApiController.java
+++ src/main/java/com/example/demo/file/api/FileApiController.java
@@ -4,13 +4,15 @@ import com.example.demo.file.domain.File;
 import com.example.demo.file.dto.FileDto;
 import com.example.demo.file.repository.FileRepository;
 import lombok.AllArgsConstructor;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -19,16 +21,22 @@ import java.util.Map;
 @AllArgsConstructor
 public class FileApiController {
 
-
     FileRepository fileRepository;
 
     @PostMapping(value="/saveImage")
-    public ResponseEntity<String> saveImage(FileDto file) {
-        try {
-            fileRepository.save(File.builder().imgFile(file.getImgFile().getBytes()).build());
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+    public ResponseEntity<String> saveImage(@RequestParam MultipartFile file) throws IOException {
+        fileRepository.save(File.builder().imgFile(file.getBytes()).build());
         return new ResponseEntity<String>("추가 완료", HttpStatus.OK);
     }
+
+    @GetMapping(value="/getByteImage")
+    public ResponseEntity<byte[]> getByteImage() {
+        File file = fileRepository.findById(1L).get();
+        byte[] imageContent = (byte[]) file.getImgFile();
+        final HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.IMAGE_PNG);
+        return new ResponseEntity<byte[]>(imageContent, headers, HttpStatus.OK);
+    }
+
+
 }
